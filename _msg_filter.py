e5ef103bd4d6d266cfbd2d48cb851eacc0ba8b8c
Abdul Salam <get2salam@gmail.com>
2026-04-17 08:47:56 +0100

    remove old secret-cleanup scripts from public repo

--- _msg_filter.py
+++
@@ -1,6 +0,0 @@
-import sys
-msg = sys.stdin.read()
-msg = msg.replace("qanoon-api", "legal-api")
-msg = msg.replace("qanoon-rag", "rag-legal-search")
-msg = msg.replace("qanoon", "legal-platform")
-sys.stdout.write(msg)
